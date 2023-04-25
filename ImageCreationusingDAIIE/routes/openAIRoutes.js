c2d56c38d63e844acbfbe1442ad6da56d2796bdf
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-04-26 01:07:25 +0530

    Delete ImageCreationusingDAIIE directory

--- ImageCreationusingDAIIE/routes/openAIRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const openAiRouter = express.Router();
-const { generateImage } = require("../controllers/openAIController");
-
-openAiRouter.route("/createImage").post(generateImage);
-
-module.exports = openAiRouter;
